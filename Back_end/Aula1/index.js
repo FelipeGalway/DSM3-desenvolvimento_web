fe8eca29a83a956e41351fb4b9b8985f010b86d5
Felipe Ferreira Rezende <felipe_legitimo@hotmail.com>
2024-04-30 20:36:38 -0300

    Arquivos e materiais de aula

--- Back_end/Aula1/index.js
+++ Back_end/Aula1/index.js
@@ -4,24 +4,45 @@ server.use(express.json());
 
 const cursos = ['Node JS', 'Python', 'React Show', 'JavaScript', 'PHP', 'React', 'VueJS'];
 
+// Middleware Local
+function checkCurso(req, res, next){
+    if(!req.body.novo_curso){
+        return res.status(400).json({error:
+        "Nome do curso é obrigatório nesse formato {'novo_curso' : 'Nome'}"});
+    }
+    
+    return next();
+};
+
+// Middleware Local
+function checkIDCurso(req, res, next){
+    const curso = cursos[req.params.index];
+    if(!curso){
+        return res.status(400).json({error:
+        "O curso não existe no ID solicitado"});
+    }
+    
+    return next();
+};
+
 server.get('/curso', (req, res)=>{
     return res.json(cursos);
 });
 
-server.get('/curso/:index', (req, res) => {
+server.get('/curso/:index', checkIDCurso, (req, res) => {
     const { index } = req.params;
 
     return res.json(cursos[index]);
 });
 
-server.post('/curso', (req, res)=>{
+server.post('/curso', checkCurso, (req, res)=>{
     const { novo_curso } = req.body;
     cursos.push(novo_curso);
 
     return res.json(cursos);
 });
 
-server.put('/curso/:index', (req, res)=>{
+server.put('/curso/:index', checkIDCurso, (req, res)=>{
     const { index } = req.params;
     const { curso } = req.body;
 
